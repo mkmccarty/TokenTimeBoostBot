b93654fe3eedf66bb9e2494c635aa9d0372dd452
Michael McCarty <mkmccarty@gmail.com>
2023-12-27 17:45:47 -0800

    Updated comments to match

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -10,18 +10,18 @@ import (
 func TestReadConfig(t *testing.T) {
 	err := ReadConfig("cfg_test.json")
 	if err != nil {
-		t.Errorf("LoadConfig() error = %v", err)
+		t.Errorf("ReadConfig() error = %v", err)
 		return
 	}
 
 	// Replace 'ExpectedValue' with the actual expected value
 	if config.DiscordToken != "discord_token" {
-		t.Errorf("LoadConfig() = %v, want %v", config.DiscordToken, "discord_token")
+		t.Errorf("ReadConfig() = %v, want %v", config.DiscordToken, "discord_token")
 	}
 	if config.DiscordAppID != "discord_app_id" {
-		t.Errorf("LoadConfig() = %v, want %v", config.DiscordAppID, "discord_app_id")
+		t.Errorf("ReadConfig() = %v, want %v", config.DiscordAppID, "discord_app_id")
 	}
 	if config.DiscordGuildID != "discord_guild_id" {
-		t.Errorf("LoadConfig() = %v, want %v", config.DiscordGuildID, "discord_guild_id")
+		t.Errorf("ReadConfig() = %v, want %v", config.DiscordGuildID, "discord_guild_id")
 	}
 }
