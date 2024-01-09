de18c22d814c84b06d28f88e22bc1bcb51e68fb2
Michael McCarty <mkmccarty@gmail.com>
2024-01-08 23:28:56 -0800

    Updated log Printf

--- main.go
+++ main.go
@@ -1061,7 +1061,7 @@ func main() {
 			err := s.ApplicationCommandDelete(s.State.User.ID, config.DiscordGuildID, v.ID)
 			log.Printf("Delete command '%v' command.", v.Name)
 			if err != nil {
-				log.Println("Cannot delete '%v' command: %v", v.Name, err)
+				log.Printf("Cannot delete '%v' command: %v\n", v.Name, err)
 			}
 		}
 	}
