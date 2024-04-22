e4144855450bbc529456f963e97215ea1007b733
Michael McCarty <mkmccarty@gmail.com>
2024-04-22 16:09:46 -0700

    feat: Add function to check for new Egg, Inc. contract data availability
    Added a new function `isNewEggIncContractDataAvailable` to check if new
    Egg, Inc. contract data is available. This function checks if the end of
    the existing contract data file matches the newly downloaded data.

--- main.go
+++ main.go
@@ -753,61 +753,131 @@ func init() {
 	})
 }
 
-func downloadEggIncContracts() {
+func isNewEggIncContractDataAvailable() bool {
+	req, err := http.NewRequest("GET", eggIncContractsURL, nil)
+	if err != nil {
+		log.Print(err)
+		return false
+	}
+
+	if _, err = os.Stat(eggIncContractsFile); err == nil {
+		// Get the current file size
+		fileInfo, err := os.Stat(eggIncContractsFile)
+		if err != nil {
+			log.Print(err)
+			return false
+		}
+
+		fileSize := fileInfo.Size()
+		rangeStart := fileSize - 1024
+		rangeEnd := fileSize + 4096
+		rangeHeader := fmt.Sprintf("bytes=%d-%d", rangeStart, rangeEnd)
+		req.Header.Add("Range", rangeHeader)
+		var client http.Client
+		resp, err := client.Do(req)
+		if err != nil {
+			return false
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return false
+		}
+
+		if len(body) > 0 {
+			// Test if the end of the the file eggIncContractsFile is the same as the body
+			file, err := os.Open(eggIncContractsFile)
+			if err != nil {
+				log.Print(err)
+				return false
+			}
+			defer file.Close()
+			// Read the last 1024 bytes from the file
+			file.Seek(-1024, io.SeekEnd)
+			fileBytes := make([]byte, 1024)
+			_, err = file.Read(fileBytes)
+			if err != nil {
+				log.Print(err)
+				return false
+			}
+			// Compare the last 1024 bytes of the file with the body
+			if bytes.Equal(fileBytes, body) && len(fileBytes) == len(body) {
+				return false
+			}
+
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+func downloadEggIncContracts() bool {
 	// Download the latest data from this URL https://raw.githubusercontent.com/carpetsage/egg/main/periodicals/data/contracts.json
 	// save it to disk and put it into an array of structs
+	if time.Since(lastContractUpdate) < 10*time.Minute {
+		log.Print("EI-Contracts. New data was updated ", lastContractUpdate)
+
+		return false
+	}
+	if !isNewEggIncContractDataAvailable() {
+		log.Print("EI-Contracts. No new data available")
+		return false
+	}
+
 	resp, err := http.Get(eggIncContractsURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return false
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return false
 	}
 
 	// Check if the file already exists
-	if _, err := os.Stat(eggIncContractsFile); err == nil {
-		// Get the current file size
-		fileInfo, err := os.Stat(eggIncContractsFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		currentSize := fileInfo.Size()
-
-		// Compare the current file size with the downloaded data size
-		if currentSize == int64(len(body)) {
-			// File size has not changed, no need to save to disk
-			log.Print("EI-Contracts. No new data to save.")
-			return
-		}
+	_, err = os.Stat(eggIncContractsFile)
+	if err == nil {
+		// Delete the file
+		err = os.Remove(eggIncContractsFile)
 	}
 
 	// Save to disk
 	err = os.WriteFile(eggIncContractsFile, body, 0644)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Save to disk
-	err = os.WriteFile(eggIncContractsFile, body, 0644)
-	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return false
 	}
 
 	// Notify bot of out new data
 	boost.LoadContractData(eggIncContractsFile)
+	lastContractUpdate = time.Now()
 	log.Print("EI-Contracts. New data loaded, length: ", int64(len(body)))
+	return true
 }
 
 func executeCronJob() {
-	if _, err := os.Stat(eggIncContractsFile); os.IsNotExist(err) {
-		downloadEggIncContracts()
-	} else {
+	if !downloadEggIncContracts() {
 		boost.LoadContractData(eggIncContractsFile)
 	}
+	/*
+	   Here's the exact cron config for the cloudflare worker that triggers the github action that updates contracts.
+	   Normal contract time is either 16 or 17 utc depending on US daylight savings.
+	   at utc 16, 17, and 18 it checks on the hour and every minute for the first 9 minutes after and then every 5 minutes the rest of the hour. The rest of the time it checks every 30 minutes. It happens rarely but sometimes contracts get released late.
+
+	   TLDR yes it checks right at contract release time and also fairly frequently for the next hour or two after contract release time and then every 30 minutes
+
+	*/
+	gocron.Every(1).Day().At("16:00:05").Do(downloadEggIncContracts)
 	gocron.Every(1).Day().At("16:00:15").Do(downloadEggIncContracts)
+	gocron.Every(1).Day().At("16:01:15").Do(downloadEggIncContracts)
+	gocron.Every(1).Day().At("16:02:15").Do(downloadEggIncContracts)
+	gocron.Every(1).Day().At("16:03:15").Do(downloadEggIncContracts)
+
 	gocron.Every(1).Day().Do(boost.ArchiveContracts)
 
 	<-gocron.Start()
