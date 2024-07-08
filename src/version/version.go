8003a8a1e0dfefdd21c371932aa9fa52ef6f4b75
Michael McCarty <mkmccarty@gmail.com>
2024-07-07 20:46:06 -0700

    ✨ Add version metadata to the application 📦
    Added Release, Version, and GitHash variables to version package.

--- src/version/version.go
+++ src/version/version.go
@@ -1,7 +1,10 @@
 package version
 
 var (
+	// Release is the current release of the application
 	Release = "3.5.0"
+	// Version is the current version of the application
 	Version string
+	// GitHash is the git hash of the commit that was used to build the application
 	GitHash string
 )
