fa6272aa68514a8744e3f0e02d5796106ca469e6
Michael McCarty <mkmccarty@gmail.com>
2024-10-14 19:44:22 -0700

    fix: Remove redundant information in the note for artifact sets

--- src/boost/stones.go
+++ src/boost/stones.go
@@ -555,7 +555,7 @@ func DownloadCoopStatusStones(contractID string, coopID string, details bool, so
 			val := fmt.Sprintf("%2.2f🚚", (collegShip-1.0)*100.0)
 			as.collegg = append(as.collegg, strings.Replace(strings.Replace(val, ".00", "", -1), ".25", "¼", -1))
 		} else {
-			artifactSets[i].note = append(artifactSets[i].note, fmt.Sprintf("SR: %2.4f(%d) - ei.sr: %2.4f  ratio:%2.4f", shippingRate, as.quantStones, (as.sr/1e15), collegShip))
+			artifactSets[i].note = append(artifactSets[i].note, fmt.Sprintf("SR: %2.4f(q:%d) - ei.sr: %2.4f  ratio:%2.4f", shippingRate, as.quantStones, (as.sr/1e15), collegShip))
 		}
 		bestTotal := 0.0
 		//bestString := ""
