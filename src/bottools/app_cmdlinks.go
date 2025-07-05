74dcd350c3c9c079d3240deaddba60fb769e282f
Michael McCarty <mkmccarty@gmail.com>
2025-07-05 09:48:55 -0700

    feat(help): Add new subcommands for changing contract start time
    The changes in this commit add support for two new subcommands to the "change-planned-start" command: "change-start offset" and "change-start timestamp". This allows users to set the planned start time for a contract using either a time offset or a specific timestamp.
    
    The commit also updates the help message to reflect these new subcommands, making it clearer for users how to set the planned start time for a contract.

--- src/bottools/app_cmdlinks.go
+++ src/bottools/app_cmdlinks.go
@@ -10,6 +10,14 @@ var commandMap = make(map[string]string)
 func UpdateCommandMap(commands []*discordgo.ApplicationCommand) {
 	for _, cmd := range commands {
 		commandMap[cmd.Name] = cmd.ID
+		// Handle subcommands
+		if len(cmd.Options) > 0 {
+			for _, option := range cmd.Options {
+				if option.Type == discordgo.ApplicationCommandOptionSubCommand {
+					commandMap[cmd.Name+" "+option.Name] = cmd.ID
+				}
+			}
+		}
 	}
 }
 
