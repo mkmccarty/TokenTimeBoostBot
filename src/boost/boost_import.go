3d1a05e476f0ffac94ee2cb37765dc220d9e2dfe
Michael McCarty <mkmccarty@gmail.com>
2025-08-03 22:00:52 -0700

    feat(boost): Adjust contract grade calculation logic
    Modifies the contract grade calculation logic in the `boost_import.go`
    file. Removes the special case handling for the "housing-boom" contract
    at grade 5, as it is no longer necessary. This simplifies the code and
    makes the grade calculation more consistent across all contracts.

--- src/boost/boost_import.go
+++ src/boost/boost_import.go
@@ -185,10 +185,6 @@ func PopulateContractFromProto(contractProtoBuf *ei.Contract) ei.EggIncContract 
 			c.Grade[grade].BasePoints = 187.5 * float64(gradeMult) * goalsCompleted
 		}
 
-		if c.ID == "housing-boom" && grade == 5 {
-			log.Printf("Contract ID is empty for contract %s", c.Name)
-		}
-
 		BTA := c.Grade[grade].EstimatedDuration.Minutes() / float64(c.MinutesPerToken)
 		c.Grade[grade].TargetTval = 3.0
 		if BTA > 42.0 {
