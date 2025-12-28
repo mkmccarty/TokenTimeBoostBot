be69b428a7630434cf0e2fe1ff60fe8f030fb5aa
Michael McCarty <mkmccarty@gmail.com>
2025-12-27 20:59:10 -0800

    🔧 fix: Adjust IHR calculation and minor text formatting

--- src/boost/estimate_time.go
+++ src/boost/estimate_time.go
@@ -238,7 +238,7 @@ func getContractEstimateString(contractID string, includeLeggySet bool) string {
 			estStrMax := c.EstimatedDurationMax.Round(time.Minute).String()
 			estStrMax = strings.TrimRight(estStrMax, "0s")
 			str += fmt.Sprintf("Leggy Set: **%s** CS:**%d**\n", estStrMax, int64(c.CxpMax))
-			str += fmt.Sprintf("-# Leggy set 50 TE, 8 IHR & 10 delivery stones sets, 5%s boost.\n", ei.GetBotEmojiMarkdown("token"))
+			str += fmt.Sprintf("-# Leggy set 50 TE, 8 IHR & 10 delivery stone sets, 5%s boost.\n", ei.GetBotEmojiMarkdown("token"))
 		}
 		if footerAboutCR && c.MaxCoopSize > 1 {
 			str += fmt.Sprintf("-# CoopSize-1 used for CR, extras **+%.0f**/%s\n",
@@ -309,7 +309,7 @@ func getContractDurationEstimate(contractEggsTotal float64, numFarmers float64, 
 			colHab:          1.0,
 			colIHR:          1.0,
 			calcMode:        modeStoneHuntMethod,
-			ihr:             7440.0 * 1.3 * 1.2 * math.Pow(1.05, 6), // improved set
+			ihr:             7440.0 * 1.3 * 1.2 * math.Pow(1.04, 6), // improved set
 			te:              0,
 		},
 		{
@@ -322,7 +322,7 @@ func getContractDurationEstimate(contractEggsTotal float64, numFarmers float64, 
 			colHab:          colleggtibleHab,
 			colIHR:          1.0,
 			calcMode:        modeStoneHuntMethod,
-			ihr:             7440.0 * 1.4 * 1.3 * math.Pow(1.05, 8), // leggacy set, Deflector w/o IHR stones
+			ihr:             7440.0 * 1.4 * 1.3 * math.Pow(1.04, 8), // leggacy set, Deflector w/o IHR stones
 			te:              0,
 		},
 		{
@@ -336,7 +336,7 @@ func getContractDurationEstimate(contractEggsTotal float64, numFarmers float64, 
 			colHab:          colleggtibleHab,
 			colIHR:          colleggtiblesIHR,
 			calcMode:        modeStoneHuntMethod,
-			ihr:             7440.0 * 1.4 * 1.3 * math.Pow(1.05, 8), // leggacy set, Deflector w/o IHR stones
+			ihr:             7440.0 * 1.4 * 1.3 * math.Pow(1.04, 8), // leggacy set, Deflector w/o IHR stones
 			te:              50,
 		},
 	}
