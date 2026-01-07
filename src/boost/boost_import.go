e35513aa6a6caa03e6e935bdc71f186ad35f4c81
Michael McCarty <mkmccarty@gmail.com>
2026-01-07 10:05:05 -0800

    🐛 Fix typo in estimated duration variable name

--- src/boost/boost_import.go
+++ src/boost/boost_import.go
@@ -243,7 +243,7 @@ func PopulateContractFromProto(contractProtoBuf *ei.Contract) ei.EggIncContract 
 			}
 		*/
 
-		c.EstimatedDuration, c.EstimatedDurationLower, c.EstimatedDurationMax, c.EstimateDurationSIAB = getContractDurationEstimate(c, c.TargetAmount[len(c.TargetAmount)-1], float64(c.MaxCoopSize), c.LengthInSeconds,
+		c.EstimatedDuration, c.EstimatedDurationLower, c.EstimatedDurationMax, c.EstimatedDurationSIAB = getContractDurationEstimate(c, c.TargetAmount[len(c.TargetAmount)-1], float64(c.MaxCoopSize), c.LengthInSeconds,
 			c.ModifierSR, c.ModifierELR, c.ModifierHabCap, debug)
 	}
 	/*
