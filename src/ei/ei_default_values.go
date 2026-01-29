03e6e25d462f36136ff83f59a7edab0c20b78e92
Michael McCarty <mkmccarty@gmail.com>
2026-01-29 11:50:39 -0800

    🔧 fix: Remove unnecessary whitespace in default values

--- src/ei/ei_default_values.go
+++ src/ei/ei_default_values.go
@@ -4,16 +4,16 @@ package ei
 const (
 	// DefaultClientVersion is the client version used for API requests
 	DefaultClientVersion = uint32(99)
-	
+
 	// DefaultPlatform is the platform type used for API requests
 	DefaultPlatform = Platform_IOS
-	
+
 	// DefaultPlatformString is the string representation of the platform
 	DefaultPlatformString = "IOS"
-	
+
 	// DefaultVersion is the app version used for API requests
 	DefaultVersion = "1.35.4"
-	
+
 	// DefaultBuild is the build number used for API requests
 	DefaultBuild = "111332"
 )
