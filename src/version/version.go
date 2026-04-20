9d8b2bfb757987d7f79f151a0bdf3f1b0e41fec0
Michael McCarty <mkmccarty@gmail.com>
2026-04-19 22:24:04 -0700

    ✨ feat: bump release from5.1 to6.0

--- src/version/version.go
+++ src/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Release is the current release of the application
-	Release = "5.1"
+	Release = "6.0"
 	// Version is the current version of the application
 	Version string
 	// GitHash is the git hash of the commit that was used to build the application
